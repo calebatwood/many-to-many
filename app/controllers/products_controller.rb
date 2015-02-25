9348dc001abc5ca3c85bfee34394c2f7b5b76d34
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-25 14:15:20 -0700

    User can create new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,9 +9,23 @@ class ProductsController < ApplicationController
   end
 
 
-    def create
-
-      ### create a product here.
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :company_id)
+  end
 
 end
