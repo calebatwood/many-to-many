4774b1483a8f165090cfd5b309b3ebdfd4b615ab
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-25 16:00:21 -0700

    User can edit products and companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -21,6 +21,19 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to companies_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def company_params
