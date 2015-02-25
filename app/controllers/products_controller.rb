47f1b8aa51b33697a09107e69aaf5bf3b8679c4a
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-25 14:23:09 -0700

    User can create new company

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path
+      redirect_to products_path
     else
       render :new
     end
